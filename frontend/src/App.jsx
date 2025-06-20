c5c9c509eca86b11628ed6e9ab0612e182680b2f
Jeff Uhrick <jjuhric@gmail.com>
2025-06-20 15:53:26 -0500

    resolving erros

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import Navbar from "./components/Navbar";
 
-import HomePage from "./pages/HomePage";
-import SignUpPage from "./pages/SignUpPage";
-import LoginPage from "./pages/LoginPage";
-import SettingsPage from "./pages/SettingsPage";
-import ProfilePage from "./pages/ProfilePage";
+import HomePage from "./pages/chat_app/HomePage";
+import SignUpPage from "./pages/chat_app/SignUpPage";
+import LoginPage from "./pages/chat_app/LoginPage";
+import SettingsPage from "./pages/chat_app/SettingsPage";
+import ProfilePage from "./pages/chat_app/ProfilePage";
+import ComingSoon from "./pages/recipe_app/ComingSoon";
 
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
@@ -43,6 +44,7 @@ const App = () => {
         <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+        <Route path="/recipe" element={authUser ? <ComingSoon /> : <Navigate to="/login" />} />
       </Routes>
 
       <Toaster />
