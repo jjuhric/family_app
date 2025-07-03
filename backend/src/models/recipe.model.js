7195871b908ab6cda0cc8a16b2812f6ac4d47af9
Jeff Uhrick <jjuhric@gmail.com>
2025-07-02 21:59:17 -0500

    adding Recipe model

---
+++ backend/src/models/recipe.model.js
@@ -0,0 +1,29 @@
+import { mongoose } from 'mongoose';
+
+const recipeSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    ingredients: {
+      type: [String],
+      required: true,
+    },
+    instructions: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Recipe = mongoose.model('Recipe', recipeSchema);
